5a0ed30e83d6bcd1a2b5281a39d18728e617209b
Palina Puchynskaya <FromApollo17@gmail.com>
2025-09-30 22:16:59 +0300

    удалила гамма коррекцию, добавила гистограммы в histogram_equalization

--- oiis_lab3.py
+++ oiis_lab3.py
@@ -2,27 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-img = Image.open('lab4_images/1.png')
+img = Image.open('lab4_images/1.png').convert('L')
 img_array = np.asarray(img)
 
 
+# метод гистограммного выравнивания (делаем гистограмму равномерной)
 def histogram_equalization(image):
-    # Преобразуем в одномерный массив
     flat = image.flatten()
 
-    # Создаем гистограмму
     histogram = np.zeros(256)
+    # Заполняем гистограмму: подсчитываем количество пикселей каждой яркости
     for pixel in flat:
         histogram[pixel] += 1
-
-    # Вычисляем кумулятивную функцию распределения (CDF)
+    # функция распределения
     cdf = histogram.cumsum()
 
-    # Нормализуем CDF к диапазону 0-255
+    # Формула линейно растягивает значения CDF на весь диапазон 0-255
     cdf_normalized = (cdf - cdf.min()) * 255 / (cdf.max() - cdf.min())
     cdf_normalized = cdf_normalized.astype(np.uint8)
 
-    # Применяем преобразование к изображению
     equalized = cdf_normalized[flat]
     equalized_image = np.reshape(equalized, image.shape)
 
@@ -33,17 +31,29 @@ def histogram_equalization(image):
 equalized_img = histogram_equalization(img_array)
 
 # Отображение результатов
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(12, 8))
 
-plt.subplot(1, 2, 1)
+plt.subplot(2, 2, 1)
 plt.imshow(img_array, cmap='gray')
 plt.title('Исходное изображение')
 plt.axis('off')
 
-plt.subplot(1, 2, 2)
+plt.subplot(2, 2, 2)
 plt.imshow(equalized_img, cmap='gray')
 plt.title('После выравнивания гистограммы')
 plt.axis('off')
+
+# для гистограмм
+# ось X (горизонтальная) — уровни яркости от 0 (чёрный) до 255 (белый),
+# ось Y (вертикальная) — количество пикселей с данным значением яркости.
+
+plt.subplot(2, 2, 3)
+plt.hist(img_array.flatten(), bins=256, range=(0, 256), color='gray')
+plt.title('Гистограмма исходного изображения')
+
+plt.subplot(2, 2, 4)
+plt.hist(equalized_img.flatten(), bins=256, range=(0, 256), color='gray')
+plt.title('Гистограмма после выравнивания')
 
 plt.tight_layout()
 plt.show()
